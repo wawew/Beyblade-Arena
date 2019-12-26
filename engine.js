74e126b5e78ed696e6679577f2e1aed68ef002f6
Fadhil Abdulkarim <fadhil@alterra.id>
2019-12-26 16:18:23 +0700

    early development on control feature

--- engine.js
+++ engine.js
@@ -3,20 +3,58 @@ let ctx = canvas.getContext("2d");
 let ballRadius = 10;
 let x = canvas.width/2;
 let y = canvas.height-30;
-let dx = 2;
-let dy = -2;
+let dx = 1;
+let dy = -1;
 
-function ball(radius, x, y, dx, dy) {}
-    this.radius = radius;
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
+// function ball() {//radius, x, y, dx, dy) {}
+//     let this.radius = ballRadius;
+//     let this.x = x;
+//     let this.y = y;
+//     let this.dx = dx;
+//     let this.dy = dy;
+// }
+
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = true;
+    }
 }
 
-function drawBall(ball()) {
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = false;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = false;
+    }
+}
+
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -24,13 +62,43 @@ function drawBall(ball()) {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const playerOne = drawBall(ball);
+    drawBall();
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
+    }
+
+    let radSpeed
+    if(rightPressed) {
+        dx += 0.2;
+        radSpeed = Math.sqrt((dx ** 2) + (dy ** 2))
+        if (radSpeed > 5){
+            dx -= 0.2;
+        }
+    }
+    else if(leftPressed) {
+        dx -= 0.2;
+        radSpeed = Math.sqrt((dx ** 2) + (dy ** 2))
+        if (radSpeed > 5){
+            dx += 0.2;
+        }
+    }
+    else if(upPressed) {
+        dy -= 0.2;
+        radSpeed = Math.sqrt((dx ** 2) + (dy ** 2))
+        if (radSpeed > 5){
+            dy += 0.2;
+        }
+    }
+    else if(downPressed) {
+        dy += 0.2;
+        radSpeed = Math.sqrt((dx ** 2) + (dy ** 2))
+        if (radSpeed > 5){
+            dy -= 0.2;
+        }
     }
     
     x += dx;
