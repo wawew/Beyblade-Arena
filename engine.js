ee97439b23dcb36b584413fa409e76d985cc6139
ulfarida <ulfah@alterra.id>
2019-12-27 12:28:53 +0700

    add backsound

--- engine.js
+++ engine.js
@@ -3,6 +3,9 @@ let ctx = canvas.getContext("2d");
 let x = canvas.width/2;
 let y = canvas.height-30;
 
+// backsound
+let backSound = document.getElementById("backsound")
+
 const paddleHeightHorz = 10;
 const paddleWidthHorz = canvas.width/2;
 const paddleWidthVert = 10;
@@ -284,6 +287,7 @@ function mainGame() {
     drawPaddle(paddleRightX, paddleRightY, paddleHeightVert, paddleWidthVert);
     showNyawa(playerOne);
     showNyawa(playerTwo);
+    backSound.play()
 
     let varControlOne = control(playerOne);
     let varControlTwo = control(playerTwo);
